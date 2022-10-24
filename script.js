d50c9e3a0d04ceb28edc2d82e026838489b122a7
Mário Moisés <moisesmario61@gmail.com>
2022-10-24 22:55:25 +0200

    Atualizando o site

---
+++ script.js
@@ -0,0 +1,16 @@
+let slide = document.querySelectorAll('.slide-cont')
+let index = 0;
+
+function next(){
+    slide[index].classList.remove('active')
+    index = (index + 1) % slide.length;
+    slide[index].classList.add('active');
+}
+
+function prev(){
+    slide[index].classList.remove('active')
+    index = (index - 1 + slide.length) % slide.length;
+    slide[index].classList.add('active');
+}
+
+setInterval(next, 3000);
